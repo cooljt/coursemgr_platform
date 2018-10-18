ca03c0f68584da0ef595625edf5af4d43c70132c
Ti Jiang <jiang.ti1@husky.neu.edu>
2018-10-18 14:35:13 -0400

    update courseMgr navigation compoent using Materical Desgin UI

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {Collapse} from "react-collapse";
 import history from "./history";
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 export default class Navigation extends React.Component {
 
     constructor(props) {
         super(props);
         this.inputChanged = this.inputChanged.bind(this);
-        this.state = {title: "",isOpened:false}
+        this.state = {title: "",anchorEl: null}
 
     }
     inputChanged(event) {
@@ -31,38 +37,38 @@ export default class Navigation extends React.Component {
                 <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
                     <div className="row" id="nav_panel_upper">
                         <div className="col-sm-1 home_center">
-                            <button className="btn btn-primary" type="button" onClick={()=>{
-                                this.state.isOpened?this.setState({isOpened:false}):this.setState({isOpened:true});
+                            <Button aria-owns={this.state.anchorEl ? 'simple-menu' : null}
+                                    aria-haspopup="true" onClick={(event)=>{
+                                this.setState({ anchorEl: event.currentTarget });
                             }}>
                                 <span className="navbar-toggler-icon"></span>
-                            </button>
+                            </Button>
+                            <Menu
+                                id="simple-menu"
+                                anchorEl={this.state.anchorEl}
+                                open={Boolean(this.state.anchorEl)}
+                                onClose={()=>this.setState({anchorEl:null})}>
+                                <MenuItem onClick={()=>history.push("/profile")}>
+                                    Profile
+                                </MenuItem>
+                                <MenuItem onClick={()=>{this.props.logoutUser();history.push("/");}}>
+                                    Logout
+                                </MenuItem>
+                            </Menu>
                         </div>
                         <div className="col-sm-2 home_center" id="title">
-                            <label>Course Manager</label>
+                            <Typography variant="h6" color="inherit">Course Manager</Typography>
                         </div>
                         <div className="col-sm-8 home_center">
-                            <input onChange={this.inputChanged} type="text" placeholder="New Course" className="form-control" />
+                            <TextField margin="normal" variant="outlined" label="New Course" onChange={this.inputChanged} className="w-100"/>
                         </div>
                         <div className="col-sm-1 home_center">
-                            <button onClick={() => this.createNewCourse()} className="btn btn-primary">
-                                <i className="fa fa-plus-circle fa-2x red"></i>
-                            </button>
+                            <Button variant="fab" mini color="secondary" onClick={() => this.createNewCourse()}>
+                                <AddIcon/>
+                            </Button>
                         </div>
                     </div>
                 </nav>
-                <Collapse className="container-fluid" isOpened={this.state.isOpened}>
-                    <div className="row bg-primary">
-                        <button className="btn btn-primary btn-block" onClick={()=>{
-                            this.props.logoutUser();
-                            history.push("/");
-                        }}>
-                            Logout
-                        </button>
-                    </div>
-                    <div className="row btn-primary">
-                        <button className="btn btn-primary btn-block" onClick={()=>history.push("/profile")}>Profile</button>
-                    </div>
-                </Collapse>
             </div>
         );
     }
